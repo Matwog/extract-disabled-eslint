0fcefc83002baaab9c895644423680ffa5d3d8d3
Gowtham Mohan <gowtham.mohan@signavio.com>
2020-08-17 20:24:42 +0200

    chore: add eslint disabled comments for testing

--- sample.js
+++ sample.js
@@ -9,6 +9,8 @@ const sample = []
 // eslint-disable-next-line import/no-webpack-loader-syntax
 // eslint-disable-next-line no-undef
 // eslint-disable-next-line array-callback-return
+/* eslint-disable import/no-webpack-loader-syntax */
+// eslint-disable-next-line import/no-webpack-loader-syntax
 /* eslint-disable */
 /* eslint-disable */
 // eslint-disable-next-line eqeqeq
@@ -16,3 +18,6 @@ const sample = []
 /* eslint-disable global-require */
 // eslint-disable-next-line new-cap
 // eslint-disable-next-line new-cap
+// eslint-disable-line
+// eslint-disable-next-line no-undef
+// eslint-disable-next-line array-callback-return
