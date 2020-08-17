88d19e575ac26125c57aa787e666abec09aff280
Gowtham Mohan <gowtham.mohan@signavio.com>
2020-08-17 12:16:11 +0200

    chore: add eslint disbaled comments for testing purposes

--- sample.js
+++ sample.js
@@ -1,6 +1,18 @@
 /* eslint-disable react/forbid-prop-types */
 
-// eslint-disable-next-line 
+// eslint-disable-next-line
 
 const sample = []
 //this is a sample comment
+// eslint-disable-line
+/* eslint-disable import/no-webpack-loader-syntax */
+// eslint-disable-next-line import/no-webpack-loader-syntax
+// eslint-disable-next-line no-undef
+// eslint-disable-next-line array-callback-return
+/* eslint-disable */
+/* eslint-disable */
+// eslint-disable-next-line eqeqeq
+// eslint-disable-next-line no-unused-vars
+/* eslint-disable global-require */
+// eslint-disable-next-line new-cap
+// eslint-disable-next-line new-cap
